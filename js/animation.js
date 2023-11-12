28dd7fefc7669c587c8ac20c8ac0cac22c21a833
xwyang457 <112594752+xwyang457@users.noreply.github.com>
2023-11-12 18:37:15 -0500

    Add fireworks animation effects

--- js/animation.js
+++ js/animation.js
@@ -26,3 +26,38 @@ setTimeout(() => {
 }, 20000); 
 
 startEmojiRain();
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const canvas = document.getElementById('fireworksCanvas');
+    const ctx = canvas.getContext('2d');
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    function createFirework() {
+        const x = Math.random() * canvas.width;
+        const y = Math.random() * canvas.height;
+        const size = Math.random() * 3 + 2;
+        const color = `hsl(${Math.random() * 360}, 100%, 50%)`;
+
+        ctx.beginPath();
+        ctx.arc(x, y, size, 0, Math.PI * 2, false);
+        ctx.fillStyle = color;
+        ctx.fill();
+    }
+
+    function animate() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        createFirework();
+        requestAnimationFrame(animate);
+    }
+
+    animate();
+
+    setTimeout(() => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }, 20000); 
+});
