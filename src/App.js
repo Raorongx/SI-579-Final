da50fd89cc2f660e399c2705f4a140f566d7cb88
xwyang457 <112594752+xwyang457@users.noreply.github.com>
2023-11-30 19:35:34 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import DateTimeDisplay from './components/DateTimeDisplay';
-import HolidayReminder from './components/HolidayReminder';
-import ReminderList from './components/ReminderList';
-import './App.css'; 
+import DateTimeDisplay from './DateTimeDisplay';
+import HolidayReminder from './HolidayReminder';
+import ReminderList from './ReminderList';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>事项提醒应用</h1>
-      </header>
-      <DateTimeDisplay />
-      <HolidayReminder />
-      <ReminderList />
+      <div className="left-panel">
+        <DateTimeDisplay />
+        {/* 可以在这里添加问候语的逻辑 */}
+      </div>
+      <div className="right-panel">
+        <HolidayReminder />
+        <ReminderList />
+      </div>
     </div>
   );
 }
