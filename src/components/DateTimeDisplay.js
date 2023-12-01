3ab5ed4ef725c85db6199526d8cc990b0de10967
xwyang457 <112594752+xwyang457@users.noreply.github.com>
2023-11-30 19:36:14 -0500

    Update DateTimeDisplay.js

--- src/components/DateTimeDisplay.js
+++ src/components/DateTimeDisplay.js
@@ -12,13 +12,19 @@ const DateTimeDisplay = () => {
     return () => clearInterval(timer);
   }, []);
 
-  const formatTime = (date) => {
+  const formatTime = date => {
     // 转换为西五区时间
     const localTime = date.toLocaleTimeString('en-US', { timeZone: 'America/New_York' });
     return `${date.toLocaleDateString()} ${localTime}`;
   };
 
-  return <div className="datetime-display">{formatTime(currentTime)}</div>;
+  return (
+    <div className="datetime-display">
+      <div>{formatTime(currentTime)}</div>
+      {/* 你可以在这里添加问候语的逻辑 */}
+    </div>
+  );
 };
 
 export default DateTimeDisplay;
+
