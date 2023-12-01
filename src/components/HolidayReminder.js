2b975df4108d2497b3c7761d80348f90c9a5b15a
xwyang457 <112594752+xwyang457@users.noreply.github.com>
2023-11-30 19:40:27 -0500

    Update HolidayReminder.js

--- src/components/HolidayReminder.js
+++ src/components/HolidayReminder.js
@@ -2,29 +2,35 @@
 import React, { useState, useEffect } from 'react';
 
 const HolidayReminder = () => {
-  const [holidays, setHolidays] = useState([]);
-  const YEAR = '2023'; // 可以根据需要更改年份
+  const [nearestHoliday, setNearestHoliday] = useState(null);
+  const YEAR = new Date().getFullYear(); // 获取当前年份
   const COUNTRY_CODE = 'US'; // 美国的国家代码
 
   useEffect(() => {
     fetch(`https://date.nager.at/Api/v2/PublicHolidays/${YEAR}/${COUNTRY_CODE}`)
       .then(response => response.json())
       .then(data => {
-        setHolidays(data);
+        // 假设返回的数据已经按日期排序
+        const today = new Date();
+        const upcomingHolidays = data.filter(holiday => new Date(holiday.date) >= today);
+        setNearestHoliday(upcomingHolidays[0]); // 设置最近的节日
       })
       .catch(error => {
         console.error('Error fetching data: ', error);
       });
-  }, []);
+  }, [YEAR, COUNTRY_CODE]);
 
   return (
     <div className="holiday-reminder">
-      <h2>美国公共假日</h2>
-      {holidays.map(holiday => (
-        <div key={holiday.date}>{holiday.localName} - {holiday.date}</div>
-      ))}
+      {nearestHoliday && (
+        <div>
+          <div>下一个节日：{nearestHoliday.localName} - {nearestHoliday.date}</div>
+          {/* 在这里添加倒计时逻辑 */}
+        </div>
+      )}
     </div>
   );
 };
 
 export default HolidayReminder;
+
