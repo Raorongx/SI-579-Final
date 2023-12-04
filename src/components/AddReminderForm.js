79e5cd3e227305e588532e295f8ac477a97d02f5
xwyang457 <112594752+xwyang457@users.noreply.github.com>
2023-12-03 20:49:29 -0500

    Update AddReminderForm.js

--- src/components/AddReminderForm.js
+++ src/components/AddReminderForm.js
@@ -3,36 +3,37 @@ import React, { useState } from 'react';
 const AddReminderForm = ({ onAdd }) => {
   const [text, setText] = useState('');
   const [date, setDate] = useState('');
+  const [category, setCategory] = useState('Personal');
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!text || !date) {
-      alert('Please fill in something at least！');
+      alert('Please fill in all fields!');
       return;
     }
-    if (typeof onAdd === 'function') {
-      onAdd({ text, date });
-    } else {
-      console.error('onAdd is not a function');
-    }
-    
+
+    onAdd({ text, date, category });
+
     setText('');
     setDate('');
+    setCategory('Personal');
   };
 
   return (
-    <div className="add-reminder-form"> {/* Added class here */}
+    <div className="add-reminder-form">
       <form onSubmit={handleSubmit}>
-        <div>
+        <div className="form-field">
+          <label>Reminder Content</label>
           <input
             type="text"
             value={text}
             onChange={(e) => setText(e.target.value)}
-            placeholder="Remind me of?"
+            placeholder="What do you need to remember?"
             required
           />
         </div>
-        <div>
+        <div className="form-field">
+          <label>Reminder Date</label>
           <input
             type="date"
             value={date}
@@ -40,7 +41,16 @@ const AddReminderForm = ({ onAdd }) => {
             required
           />
         </div>
-        <button type="submit">Add a reminder</button>
+        <div className="form-field">
+          <label>Category</label>
+          <select value={category} onChange={(e) => setCategory(e.target.value)}>
+            <option value="Work">Work</option>
+            <option value="Academic">Academic</option>
+            <option value="Personal">Personal</option>
+            <option value="Health">Health</option>
+          </select>
+        </div>
+        <button type="submit">Add Reminder</button>
       </form>
     </div>
   );
